b156924659a30b562c3de9670b565faed611341a
Jean-Yves Lalanne <jylalanne@demboost.com>
2023-11-03 11:25:13 +0100

    CONF back is now able to serve static folder

--- src/loaders/express.ts
+++ src/loaders/express.ts
@@ -1,3 +1,5 @@
+import fs from 'fs';
+import path from 'path';
 import express from 'express';
 import cors from 'cors';
 import config from 'config';
@@ -5,6 +7,7 @@ import routes from '@/api';
 import { ExpressConfig } from '@/models/config';
 
 const expressConfig: ExpressConfig = config.get('express');
+const rootDir = findRootDir(__dirname);
 
 const corsOptions = {
   origin: true,
@@ -18,4 +21,34 @@ export default ({ app }: { app: express.Application }) => {
   }
   // Application API routes
   app.use(routes());
+
+  // Application API routes
+  app.use('/api', routes());
+
+  if (rootDir) {
+    const staticPath = path.join(rootDir, 'static');
+    app.use(express.static(staticPath, { maxAge: '1d' }));
+    app.get('*', function (_, res) {
+      res.sendFile(path.join(staticPath, 'index.html'));
+    });
+  } else {
+    throw new Error('Root directory not found.');
+  }
 };
+
+/**
+ * Recursive search for root directory. The recursive function search for
+ * the file package.json
+ * @param currentDir
+ * @returns
+ */
+function findRootDir(currentDir: string): string | null {
+  if (fs.existsSync(path.join(currentDir, 'package.json'))) {
+    return currentDir;
+  }
+  const parentDir = path.join(currentDir, '..');
+  if (parentDir === currentDir) {
+    return null;
+  }
+  return findRootDir(parentDir);
+}
